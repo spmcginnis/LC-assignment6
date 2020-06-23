195255784d1e044a49e7dc2f4234d134d1fa14b9
majining <majining@users.noreply.github.com>
2020-06-23 09:38:13 -0500

    changed to using fetched data

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -9,14 +9,37 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
 
   constructor() {
-    this.sourceList = [
-      new Satellite('SiriusXM', 'Communication', '2009-03-21', 'LOW', true),
-      new Satellite('Cat Scanner', 'Imaging', '2012-01-05', 'LOW', true),
-      new Satellite('Weber Grill', 'Space Debris', '1996-03-25', 'HIGH', false),
-      new Satellite('GPS 938', 'Positioning', '2001-11-01', 'HIGH', true),
-      new Satellite('ISS', 'Space Station', '1998-11-20', 'LOW', true)
-    ]
+    this.sourceList = [];
+    let satelittesURL = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+    window.fetch(satelittesURL).then( function(response){
+      response.json().then( function(data) {
+
+        let fetchedSat = data.satellites;
+
+        for ( let i=0; i<fetchedSat.length; i++) {
+          let fsi = fetchedSat[i]
+          let satObj: Satellite = new Satellite(fsi.name, fsi.type, fsi.launchDate, fsi.orbitType, fsi.operational);
+          this.sourceList.push(satObj)
+        }
+        this.displayList = this.sourceList.slice(0);
+      }.bind(this) )
+    }.bind(this) )
   }
+
+  // search(searchTerm:string): void {
+  //   let matchingSatellites: Satellite[] = [];
+  //   searchTerm = searchTerm.toLowerCase();
+  //   for(let i=0; i<this.sourceList.length; i++) {
+  //     let name = this.sourceList[i].name.toLowerCase();
+  //     if (name.indexOf(searchTerm) >= 0) {
+  //       matchingSatellites.push(this.sourceList[i])
+  //     }
+  //   }
+  //   this.displayList = matchingSatellites;
+
+  // }
 }
